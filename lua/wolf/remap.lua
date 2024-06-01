6043df4ab682d5075318b275355886e7214ad7ba
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-05-31 23:55:47 -0400

    With coc

--- lua/wolf/remap.lua
+++ lua/wolf/remap.lua
@@ -22,23 +22,22 @@ map('n', '<C-d>', '<C-d>zz', opts)
 map('n', '<C-u>', '<C-u>zz', opts)
 
 -- Split window vertically
-map('n', '<Leader>sv', ':vsplit<CR>', opts) 
+map('n', '<Leader>sv', ':vsplit<CR>', opts)
 
 -- Split window horizontally
-map('n', '<Leader>sh', ':split<CR>', opts)                                                                                                                                      
+map('n', '<Leader>sh', ':split<CR>', opts)
 -- Navigate splits easily using Ctrl + {h,j,k,l}
 map('n', '<C-h>', '<C-w>h', opts)
 map('n', '<C-j>', '<C-w>j', opts)
 map('n', '<C-k>', '<C-w>k', opts)
 map('n', '<C-l>', '<C-w>l', opts)
 
--- Resize splits more easily 
+-- Resize splits more easily
 resize_amount = 5
-map('n', '<Leader>hh', ':vertical resize -' .. resize_amount .. '<CR>', opts) 
+map('n', '<Leader>hh', ':vertical resize -' .. resize_amount .. '<CR>', opts)
 map('n', '<Leader>ll', ':vertical resize +' .. resize_amount .. '<CR>', opts)
-map('n', '<Leader>kk', ':resize +' .. resize_amount .. '<CR>', opts) 
-map('n', '<Leader>jj', ':resize -' .. resize_amount .. '<CR>', opts) 
+map('n', '<Leader>kk', ':resize +' .. resize_amount .. '<CR>', opts)
+map('n', '<Leader>jj', ':resize -' .. resize_amount .. '<CR>', opts)
 
 -- Close the current view
 map('n', '<C-w>', ':q<CR>', opts)
-
