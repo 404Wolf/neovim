9b338d7ed17c041a316c7d30707d5b60aa694dc3
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-02 10:43:48 -0400

    Update binds

--- lua/wolf/keybinds/plugins.lua
+++ lua/wolf/keybinds/plugins.lua
@@ -9,22 +9,21 @@ utils.map("n", "<C-m>", "<cmd>AerialToggle!<CR>", {})
 utils.map("n", "<Leader>as", ":ASToggle<CR>", {})
 
 -- Comments
-utils.map('n', '<C-/>', 'gc', opts)
+utils.map("n", "<C-/>", "gc", opts)
 
 -- Docstrings
-utils.map('n', '<C-j>', ':DogeGenerate<CR>', opts)
+utils.map("n", "<C-j>", ":DogeGenerate<CR>", opts)
 
 -- Neotree
-utils.map('n', '<C-n>', ':Neotree toggle<CR>', opts)
+utils.map("n", "<C-n>", ":Neotree toggle<CR>", opts)
 
 -- Hop
-utils.map('n', '<Leader>hw', ':HopWord<CR>', opts)
-utils.map('n', '<Leader>hc', ':HopCamelCase<CR>' , opts)
-utils.map('n', '<Leader>hl', ':HopLine<CR>', opts)
+utils.map("n", "<Leader>hw", ":HopWord<CR>", opts)
+utils.map("n", "<Leader>hc", ":HopCamelCase<CR>", opts)
+utils.map("n", "<Leader>hl", ":HopLine<CR>", opts)
 
 -- LSP
-utils.map('n', 'grn', 'lua vim.lsp.buf.rename()', opts)
-utils.map('n', 'gra', 'lua vim.lsp.buf.code_action()', opts)
-utils.map('n', 'grr', 'lua vim.lsp.buf.references()', opts)
-utils.map('i', '<C-S>', 'lua vim.lsp.buf.signature_help()', opts)
-
+utils.map("n", "grn", ":lua vim.lsp.buf.rename()<CR>", opts)
+utils.map("n", "gra", ":lua vim.lsp.buf.code_action()<CR>", opts)
+utils.map("n", "grr", ":lua vim.lsp.buf.references()<CR>", opts)
+utils.map("i", "<C-S>", ":lua vim.lsp.buf.signature_help()<CR>", opts)
