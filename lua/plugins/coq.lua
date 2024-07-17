4a467853bad439fc949fe0207f694993ad9dc8bb
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-07-16 20:48:21 -0400

    Fix bind

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -1,7 +1,7 @@
 vim.g.coq_settings = {
 	auto_start = "shut-up", -- if you want to start COQ at startup
 	keymap = {
-		jump_to_mark = nil,
+		jump_to_mark = "asdfghjkl",
 	},
 	xdg = true,
 	clients = {
